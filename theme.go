277ed9c4b45b173fb4a5bd6097410b1428b6785a
louismerlin <louis.merlin@epfl.ch>
2016-11-27 11:32:05 +0100

    Write working project, no 'light' yet

--- theme.go
+++ theme.go
@@ -1,53 +1,141 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
 	"strings"
 )
 
-type Flavor struct {
-	base00 []string
-	base01 []string
-	base02 []string
-	base03 []string
-	base04 []string
-	base05 []string
-	base06 []string
-	base07 []string
-	base08 []string
-	base09 []string
-	base0A []string
-	base0B []string
-	base0C []string
-	base0D []string
-	base0E []string
-	base0F []string
+func extractData(file os.FileInfo, flavor map[string]string) error {
+	usr, err := user.Current()
+	if err != nil {
+		return (err)
+	}
+
+	home := usr.HomeDir
+
+	dir := home + "/.theme/templates/" + file.Name()
+	input, err := ioutil.ReadFile(dir)
+	if err != nil {
+		return (err)
+	}
+
+	var dat map[string]interface{}
+	if err := json.Unmarshal(input, &dat); err != nil {
+		return err
+	}
+
+	fmt.Print(dat["name"], ": ")
+	err = readAndReplace(dat, flavor)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("ok")
+	}
+
+	return nil
+}
+
+func readAndReplace(file map[string]interface{}, flavor map[string]string) error {
+	loc := file["location"].(string)
+	input, err := ioutil.ReadFile(loc)
+	if err != nil {
+		return err
+	}
+
+	lines := file["lines"].([]interface{})
+
+	for _, line := range lines {
+		line := line.(map[string]interface{})
+		prev := line["prev"].(string)
+		li := strings.Split(string(input), "\n")
+		for i, l := range li {
+			if strings.Contains(l, prev) {
+				next := line["next"].(string)
+				color := flavor[line["color"].(string)]
+				li[i] = fmt.Sprintf(next, color)
+			}
+		}
+		output := strings.Join(li, "\n")
+		err = ioutil.WriteFile(loc, []byte(output), 0644)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func flavorNotFound(flavor string, flavors []os.FileInfo) {
+	if flavor != "" {
+		fmt.Println("Flavor not found : ", flavor)
+	}
+
+	for _, fl := range flavors {
+		fmt.Println(fl.Name()[:len(fl.Name())-5])
+	}
 }
 
 func main() {
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
+	args := os.Args[1:]
 
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-
-	input, err := ioutil.ReadFile("myfile")
+	usr, err := user.Current()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	lines := strings.Split(string(input), "\n")
+	home := usr.HomeDir
 
-	for i, line := range lines {
-		if strings.Contains(line, "]") {
-			lines[i] = "LOL"
+	dir := home + "/.theme/flavors/"
+	flavors, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if len(args) == 0 {
+		flavorNotFound("", flavors)
+		return
+	}
+
+	var flavor map[string]string
+	for _, fl := range flavors {
+		if fl.Name()[:len(fl.Name())-5] == args[0] {
+			dir = home + "/.theme/flavors/" + fl.Name()
+			input, err := ioutil.ReadFile(dir)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if err := json.Unmarshal(input, &flavor); err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
 	}
-	output := strings.Join(lines, "\n")
-	err = ioutil.WriteFile("myfile", []byte(output), 0644)
+
+	if flavor == nil {
+		flavorNotFound(args[0], flavors)
+		return
+	}
+
+	dir = home + "/.theme/templates/"
+	templates, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	for _, template := range templates {
+		err := extractData(template, flavor)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
 }
